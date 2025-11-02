1950ad0e8dec747e73364ba36c565bd1999f4bfe
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-02 14:41:15 +0900

    [Bronze V] Title: 사분면 고르기, Time: 172 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int Y = scanner.nextInt();
+        if(X>0 && Y>0){
+            System.out.println("1");
+        }
+        else if(X>0 && Y<0){
+            System.out.println("4");
+        }
+        else if(X<0 && Y>0){
+            System.out.println("2");
+        }else System.out.println("3");
+
+
+        scanner.close();
+    }
+
+}
+
