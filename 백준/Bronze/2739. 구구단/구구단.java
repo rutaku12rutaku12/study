4d2059777917d424e43af8592a145b8092e8873b
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-05 10:22:18 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18608 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
         int A = scan.nextInt();
         int i;
         for( i = 1; i <= 9 ; i++){
             System.out.println(A+" * "+i+" = "+A*i);
         }
-
         scan.close();
     }
 
