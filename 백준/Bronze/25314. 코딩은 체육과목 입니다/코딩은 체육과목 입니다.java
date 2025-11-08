127e7afabcf734da948dcc6c2323847d8c9d81a8
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-08 20:49:50 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i =0; i<T/4; i++)
+        {
+        System.out.print("long ");}
+        System.out.print("int");
+        //첫 번째 줄에는 문제의 정수
+        //$N$이 주어진다.
+        //$(4\le N\le 1\, 000$;
+        //$N$은
+        //$4$의 배수
+        // 4
+        //예제 출력 1
+        //long int
+
+
+
+
+
+        scanner.close();
+    }
+}
