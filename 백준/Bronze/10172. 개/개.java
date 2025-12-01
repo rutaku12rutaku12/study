8e58047feb447207a9cd6b278f636127ee30268b
rutaku12rutaku12 <chookyuho@gmail.com>
2025-12-01 09:58:05 +0900

    [Bronze V] Title: 개, Time: 160 ms, Memory: 17364 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+        scan.close();
+    }
+}
