685641b68cea749a99f52f6b7a3c2fd7b6c86290
rutaku12rutaku12 <chookyuho@gmail.com>
2025-12-01 09:57:33 +0900

    [Bronze V] Title: 고양이, Time: 164 ms, Memory: 17340 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+
+        scan.close();
+    }
+}
