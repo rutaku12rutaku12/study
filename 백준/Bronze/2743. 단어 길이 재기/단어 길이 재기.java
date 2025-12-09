4c4ae6e29b648371d252263f5aa4ff36fb077abe
rutaku12rutaku12 <chookyuho@gmail.com>
2025-12-09 14:24:11 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String S = scan.nextLine();
+        System.out.println(S.length());
+        scan.close();
+    }
+}
