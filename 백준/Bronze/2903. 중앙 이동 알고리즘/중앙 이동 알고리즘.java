4d96dc3a370242bdd3299ed5e7d7e69e0346a11f
rutaku12rutaku12 <chookyuho@gmail.com>
2026-01-04 21:34:51 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		double N = Integer.parseInt(br.readLine());
+		double j = Math.pow(((Math.pow(2,N))+1),2);
+		int q = (int)j;
+		bw.write(q+"");
+		bw.flush();
+		bw.close();
+	}
+}
