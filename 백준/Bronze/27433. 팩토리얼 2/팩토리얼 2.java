5d820e64f5cb287db0210697fd487f1169c26717
rutaku12rutaku12 <chookyuho@gmail.com>
2026-01-14 22:06:19 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 108 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main{
+	public static void main(String[] args ) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		long N = Integer.parseInt(br.readLine());
+		long sum = 1;
+		for(int i = 1; i<=N; i++) {
+			sum*=i;
+		}
+		bw.write(sum+"");
+		bw.flush();
+		bw.close();
+	}
+}
