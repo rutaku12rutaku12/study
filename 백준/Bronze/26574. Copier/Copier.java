79e81f96c16d18607c58cc0173629353be65b3cf
rutaku12rutaku12 <chookyuho@gmail.com>
2026-01-14 22:28:41 +0900

    [Bronze V] Title: Copier, Time: 120 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main{
+	public static void main(String[] args ) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		for(int i =0; i<N; i++) {
+			int A = Integer.parseInt(br.readLine());
+			bw.write(A+" "+A+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
