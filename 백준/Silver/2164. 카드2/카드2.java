d3021ff95152d4336646f80b8bcf2345757dd62c
rutaku12rutaku12 <chookyuho@gmail.com>
2026-01-19 18:45:03 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 45664 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i=1; i<=N;i++) {
+			queue.add(i);
+		}
+			while(queue.size()>1) {
+				queue.remove();
+				queue.add(queue.remove());
+			}
+		bw.write(queue.peek()+"");
+		bw.flush();
+		bw.close();
+	}
+}
