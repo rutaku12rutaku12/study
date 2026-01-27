a2d8ce3509398a31df4683d965ea933b38b8643a
rutaku12rutaku12 <chookyuho@gmail.com>
2026-01-27 17:53:54 +0900

    [Bronze II] Title: 벌집, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int a = Integer.parseInt(br.readLine());
+		int max =1;
+		int lay = 1;
+		while(a>max) {
+			max+=lay*6;
+			lay++;
+		}
+		bw.write(lay+"");
+		bw.flush();
+		bw.close();
+	}
+}
