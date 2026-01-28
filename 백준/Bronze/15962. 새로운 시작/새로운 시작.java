bfe920d22799045161aea06f82d248c2d6c008d1
rutaku12rutaku12 <chookyuho@gmail.com>
2026-01-28 16:12:14 +0900

    [Bronze V] Title: 새로운 시작, Time: 88 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("파이팅!!");
+		bw.flush();
+		bw.close();
+	}
+}
