487b72d02042c7e77c41bb4205a75e4770990b19
rutaku12rutaku12 <chookyuho@gmail.com>
2026-02-04 22:06:39 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 132 ms, Memory: 15832 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int i=0; i<T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int sum =0;
+			int k = (a*a);
+			while(k>0) {
+				k-=b*b;
+				sum++;
+			}
+			bw.write(sum+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
