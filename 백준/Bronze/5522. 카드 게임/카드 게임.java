24ffc1107df6e60e8ceb66ecb9c29b77dcb69a0c
rutaku12rutaku12 <chookyuho@gmail.com>
2026-02-15 19:51:03 +0900

    [Bronze V] Title: 카드 게임, Time: 112 ms, Memory: 14268 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		//int T = Integer.parseInt(br.readLine());
+		int sum=0;
+		for(int i =1; i<=5; i++) {
+			int p = Integer.parseInt(br.readLine());
+			sum+=p;
+		}
+		bw.write(sum+"");
+		bw.flush();
+		bw.close();
+	}
+}
