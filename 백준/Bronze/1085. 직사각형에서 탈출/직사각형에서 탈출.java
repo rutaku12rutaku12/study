521286086378dffa76953ad292e9857010425778
rutaku12rutaku12 <chookyuho@gmail.com>
2026-03-10 19:23:25 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{ 
+	static BufferedWriter bw;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		int sum1 =Integer.MAX_VALUE;
+		int sum2= Integer.MAX_VALUE;
+		
+			sum1 =Math.min(w-x, h-y);
+			sum2 =Math.min(x, y);
+		int k = Math.min(sum1, sum2);
+		bw.write(k+"");
+		bw.write("");
+		bw.flush();
+		bw.close();
+	}
+}
