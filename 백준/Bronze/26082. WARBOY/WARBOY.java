71f1eb8b0332b9bb49e96dc827fcf3d85eb4adfe
rutaku12rutaku12 <chookyuho@gmail.com>
2026-03-11 17:47:44 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{ 
+	static BufferedWriter bw;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		//A 가격 B 성능 C 가격 D 성능 
+		int d = 0;
+		d = 3*B*C/A;
+		bw.write(d+"");
+		bw.flush();
+		bw.close();
+	}
+}
