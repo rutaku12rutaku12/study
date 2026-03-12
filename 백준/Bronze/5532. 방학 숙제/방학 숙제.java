d61742a171c20e627ca9426ff1bda5395b026a19
rutaku12rutaku12 <chookyuho@gmail.com>
2026-03-12 17:29:01 +0900

    [Bronze IV] Title: 방학 숙제, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{ 
+	static BufferedWriter bw;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int L = Integer.parseInt(br.readLine());
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		int D = Integer.parseInt(br.readLine());
+		int math =0;
+		int kor = 0;
+		while(B>0) {
+			B-=D;
+			math++;
+		}
+		while(A>0) {
+			A-=C;
+			kor++;
+		}
+		bw.write(L-Math.max(math, kor)+"");
+		bw.flush();
+		bw.close();
+	}
+}
