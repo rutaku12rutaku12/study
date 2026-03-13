a424bbdfbb0f3b0a99fd5462bc6696c32ada33cf
rutaku12rutaku12 <chookyuho@gmail.com>
2026-03-13 16:19:13 +0900

    [Bronze III] Title: 앵그리 창영, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{ 
+	static BufferedWriter bw;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		for(int i=0; i<a;i++) {
+			int k = Integer.parseInt(br.readLine());
+			if(k*k<=b*b+c*c) {
+				bw.write("DA\n");
+			}
+			else bw.write("NE\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
