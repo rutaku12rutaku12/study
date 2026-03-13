df8fe375c3640bb987b5c28316dbd2ccc2788a93
rutaku12rutaku12 <chookyuho@gmail.com>
2026-03-13 15:41:17 +0900

    [Bronze IV] Title: OO0OO, Time: 104 ms, Memory: 14048 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main{ 
+	static BufferedWriter bw;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		br.readLine();
+		br.readLine();
+		br.readLine();
+		for(int i=0; i<10000;i++) {
+			
+			bw.write("-1"+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
