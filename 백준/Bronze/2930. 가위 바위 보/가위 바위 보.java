aedfa5c6f90cfe81c62b1622e42ad7c27a5abd63
rutaku12rutaku12 <chookyuho@gmail.com>
2026-03-16 20:36:23 +0900

    [Bronze I] Title: 가위 바위 보, Time: 116 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Bronze/2930. 가위 바위 보/가위 바위 보.java
+++ 백준/Bronze/2930. 가위 바위 보/가위 바위 보.java
@@ -8,46 +8,51 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int R = Integer.parseInt(br.readLine());
-        String s= br.readLine();
-        int N  = Integer.parseInt(br.readLine());
-
-        String[] f = new String[N];
-        for(int i=0; i<N;i++){
-            f[i]=br.readLine();
+        int A = Integer.parseInt(br.readLine()); // 가위바위보 횟수
+        String a = br.readLine(); // 내가 낸 가위바위보
+        char [] arr = new char[A];
+        for(int i =0;i<A;i++) {
+            arr[i] = a.charAt(i);
         }
-        int score=0;
-        int max=0;
+        int N = Integer.parseInt(br.readLine()); // 친구 명수
+        int score = 0;
+        int max =0;
+        String[] fra = new String[N];
+        for(int i=0; i<N;i++){
+            fra[i]= br.readLine();
+        }
+        for(int i=0;i<A;i++){
+            int P = 0;
+            int S =0;
+            int R =0;
+            for(int k=0; k<N;k++){
+                char me = arr[i];
 
-        for(int i=0;i<R;i++){
-            int S=0;
-            int P=0;
-            int Rr=0;
-            char me =s.charAt(i);
+                    char fr = fra[k].charAt(i);
+                   // bw.write(me+" "+fr+"///");
 
-            for(int j=0; j<N;j++){
-                
-                char fr = f[j].charAt(i);
+                    if(me==fr){
+                        score++;
+                    }
+                    else if ((me=='P' && fr=='R')|| (me=='R'&& fr=='S')|| (me=='S'&&fr=='P')){
+                        score+=2;
 
-                if(me==fr) {
-                    score++;
-                }else if((me=='S'&&fr=='P')||(me=='P'&&fr=='R')||(me=='R'&&fr=='S')){
-                    score+=2;
-                }
-                 if(fr=='S'){
-                    S+=1;
-                    Rr+=2;
-                }
-                else if(fr=='P'){
-                    P+=1;
-                    S+=2;
-                }
-                else{
-                    Rr+=1;
-                    P+=2;
-                }
-            }
-            max+=Math.max(S,Math.max(P,Rr));
+                    }
+                    if(fr=='R'){
+                        R++;
+                        P+=2;
+                    }
+                    else if(fr=='P'){
+                        P++;
+                        S+=2;
+                    }else {
+                        S++;
+                        R+=2;
+                    }
+                    //bw.write(P+" "+S+" "+R+"\n");
+
+            }max +=Math.max(P,Math.max(S,R));
+
         }
         bw.write(score+"\n");
         bw.write(max+"");
