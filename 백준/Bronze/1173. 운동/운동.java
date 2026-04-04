a7caa78fd5ffe25b1cc94c8ade224d06dbb7e507
rutaku12rutaku12 <chookyuho@gmail.com>
2026-04-05 00:56:00 +0900

    [Bronze II] Title: 운동, Time: 112 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/1173. 운동/운동.java
+++ 백준/Bronze/1173. 운동/운동.java
@@ -1,36 +1,66 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = sc.nextInt();
-        int m = sc.nextInt();
-        int M = sc.nextInt();
-        int T = sc.nextInt();
-        int R = sc.nextInt();
 
-        // 운동 자체가 불가능한 경우
-        if (m + T > M) {
-            System.out.println(-1);
-            return;
-        }
-
-        int cur = m;
-        int exercise = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+        // 1분마다 선택 : 휴식 or 운동
+        // 운동 => 맥박(T) 상승
+        // 맥박 최대치 M
+        // 휴식 => 맥박(R) 감소
+        // 맥박 최소치 m
+        int re = m;
         int time = 0;
+        int set = 0;
 
-        while (exercise < N) {
-            if (cur + T <= M) {
-                cur += T;
-                exercise++;
-            } else {
-                cur -= R;
-                if (cur < m) cur = m;
+        while(re>=m && re<=M) {
+            if(m+T>M){
+                bw.write("-1");
+                bw.flush();
+                return;
             }
-            time++;
+            if(set==N){
+                bw.write(time+"");
+                bw.flush();
+                return;
+            }
+            if(re+T<=M){
+                re+=T;
+                time++;
+                set++;
+               // bw.write("운동 \n");
+            }
+            else if(re+T>M){
+                if(re-R<=m){
+                    re=m;
+                }else {
+                    re -= R;
+                }
+                time++;
+              //  bw.write("휴식 \n");
+            }
+            else{
+                bw.write("-1");
+                bw.flush();
+                return;
+            }
         }
+        bw.flush();
+        bw.close();
 
-        System.out.println(time);
     }
 }
