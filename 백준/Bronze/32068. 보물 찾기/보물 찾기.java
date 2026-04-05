40403f23be7877044988be7b323e5752b4a4b93d
rutaku12rutaku12 <chookyuho@gmail.com>
2026-04-05 23:28:28 +0900

    [Bronze II] Title: 보물 찾기, Time: 612 ms, Memory: 41628 KB, Score: 48 point -BaekjoonHub

---
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i =0; i<T;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int p = S; // 위치
+            int set =1; //
+            int n = 0; //
+            int k = -1;
+            while(L!=p || p!=R){
+               // bw.write(n+1+"단계 ");
+               // bw.write("위치:"+p+" ");
+                if(L==p){
+                   // bw.write("위치:"+p+" ");
+                    bw.write(set+"\n");
+                    bw.flush();
+                    break;
+                }
+                else if(R==p){
+                   // bw.write("위치:"+p+" ");
+                    bw.write(set+"\n");
+                    bw.flush();
+                    break;
+                }
+                set++;
+                n++;
+                k*=-1;
+                p+=n*k;
+                //bw.write("위치:"+p+"\n ");
+            }
+
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
