3d84868f36b553dc1a96371122cd8098087da628
rutaku12rutaku12 <chookyuho@gmail.com>
2026-04-11 22:53:29 +0900

    [Bronze II] Title: 연속구간, Time: 116 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Bronze/2495. 연속구간/연속구간.java
+++ 백준/Bronze/2495. 연속구간/연속구간.java
@@ -1,28 +1,34 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int t = 0; t < 3; t++) {
-            String s = br.readLine();
-
-            int max = 1;
-            int count = 1;
-
-            for(int i = 1; i < s.length(); i++) {
-                if(s.charAt(i) == s.charAt(i-1)) {
+        for(int i=0;i<3;i++){
+            String a = br.readLine();
+            int max=1;
+            int count=1;
+            for(int j=1;j<a.length();j++){
+                if(a.charAt(j)==a.charAt(j-1)){
                     count++;
-                } else {
-                    count = 1;
+                }else{
+                    count=1;
                 }
-
-                if(count > max) {
-                    max = count;
+                if(count>max){
+                    max=count;
                 }
-            }
-
-            System.out.println(max);
+            }bw.write(max+"\n");
         }
+
+        bw.flush();
+        bw.close();
+
     }
 }
