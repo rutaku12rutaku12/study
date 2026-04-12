3e1b7aa4cde3e105cd954c77e198a2aeb8be07a7
rutaku12rutaku12 <chookyuho@gmail.com>
2026-04-13 02:39:01 +0900

    [Bronze I] Title: APC는 왜 서브태스크 대회가 되었을까?, Time: 116 ms, Memory: 15852 KB, Score: 140 point -BaekjoonHub

--- 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.java
+++ 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.java
@@ -16,25 +16,40 @@ public class Main {
         int L = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int sum =0;
+        List <Integer> Harr = new ArrayList<>();
+        List <Integer> Earr = new ArrayList<>();
         for(int i=0; i<N;i++){
-            if(K==0){
-                return;
-            }
             StringTokenizer stt = new StringTokenizer(br.readLine());
             int easy = Integer.parseInt(stt.nextToken());
             int hard = Integer.parseInt(stt.nextToken());
-
             if(L-hard>=0){
+                Harr.add(hard);
+            }
+            else if(L-easy>=0){
+                Earr.add(easy);
+            }
+        }
+        for(int i=0; i<Harr.size();i++){
+            if(K==0){
+                break;
+            }
+            if(Harr.get(i)>0){
                 sum+=140;
                 K--;
             }
-            else if (L-easy>=0){
+        }
+        for(int i=0; i<Earr.size();i++){
+            if(K==0){
+                break;
+            }
+            if(Earr.get(i)>0){
                 sum+=100;
                 K--;
             }
-
         }
-        bw.write(sum+"");
+        bw.write(sum+"\n");
+        //bw.write(Harr+"\n"+Earr+"\n");
+        //bw.write(Harr.size()*140+Earr.size()*100+"");
         bw.flush();
         bw.close();
 
