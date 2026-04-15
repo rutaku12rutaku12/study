fb89766829a98442ceac9ca5b76c5acfe3b34ab9
rutaku12rutaku12 <chookyuho@gmail.com>
2026-04-15 18:54:35 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {0,0};
+        while(money>=5500){
+            money-=5500;
+            answer[0]+=1;
+            
+        }answer[1]=money;
+        return answer;
+    }
+}
