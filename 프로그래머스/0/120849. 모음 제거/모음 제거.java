830522df06d806d993520c8ba8758cfc6d205015
rutaku12rutaku12 <chookyuho@gmail.com>
2026-04-18 21:13:53 +0900

    [level 0] Title: 모음 제거, Time: 6.60 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length();i++){
+            char c = my_string.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                
+            }
+            else {
+                String a = c+"";
+                answer+=a;
+            }
+        }
+        return answer;
+    }
+}
